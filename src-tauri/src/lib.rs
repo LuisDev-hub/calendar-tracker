c7aeda72d22b876115a1be90614da431d42e2296
LuisDev-hub <luis.ramirez.developer@proton.me>
2026-07-02 00:26:48 -0600

    Set sqllite, and webkitgtk variable

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
     tauri::Builder::default()
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
